57bfe86b640d761acb4a4309a7be4be3451f3d8c
Victor Miranda <vmm@cin.ufpe.br>
2019-11-12 12:46:05 -0300

    q4

--- Projeto2.r
+++ Projeto2.r
@@ -3,7 +3,7 @@
 #Vitor Miranda - vmm
 
 #Q1 Descarregue o arquivo .csv da planilha e imprima o dataframe obtido exatamente do jeito que ele se encontra.
-df <- read.csv(file="ET586-Projeto2/detalhes-albuns.csv", header=TRUE, sep=",", encoding = "UTF-8")
+df <- read.csv(file="detalhes-albuns.csv", header=TRUE, sep=",", encoding = "UTF-8")
 print("Q1:")
 print(df)
 
@@ -33,12 +33,12 @@ print(moda)
 
 #Q3 Faça uma função que retorna os nomes dos artistas que lançaram álbuns nos dois anos (ou seja, o mesmo artista lançou um ou mais álbuns em 2018 e em 2019).
 albunsxartistas <- function() {
-
+  #vetores com artistas que laçaram álbuns em 2018 e 2019
   v2018 = df[df$Ano == 2018,][["Artista"]]
   v2019 = df[df$Ano == 2019,][["Artista"]]
 
   artistas = vector()
-
+  #varre o vetor v2018 e verifica se o artista está contido no vetor 2019
   for (i in v2018){
     if (i %in% v2019) {
        artistas = c(artistas, i)
@@ -51,11 +51,29 @@ print("Artistas que lançaram albums em 2018 e 2019:")
 print(albunsxartistas())
 
 #Q4Faça uma função que retorne qual artista possui o menor desvio padrão nas vendas.
-artistaMenorDesvio <- function(ano) {
+artistaMenorDesvio <- function() {
+  # cria dataframe com as frequencias de cada artista, eliminando os que aparecem apenas 1 vez
+  freq = as.data.frame(table(df["Artista"]))
+  freq = freq[(freq$Freq > 1),]
   
-  return()
+  #cria um vetor para armazenar os DP de cada artista
+  vDP = vector()
+  # armazena no vetor
+  for (artist in as.vector(freq[[1]])) {
+    vDP = c(vDP, sd(df[df$Artista == artist,][[5]]))
+    
+  }
+  #cria um novo dataframe com o artista e seu DP
+  dfDP = data.frame(freq[1], vDP)
+  
+  #nomeia colunas para ordenar pelo desvio padrao
+  colnames(dfDP)[1] = "Artista"
+  colnames(dfDP)[2] = "DesvioPadrao"
+  dfDP = dfDP[order(dfDP$DesvioPadrao),]
+  #retorna o artista da primeira posicao do novo dataframe
+  return(as.character(dfDP[[1,1]]))
 }
-print("Q4")
+print("Q4:")
 print(artistaMenorDesvio())
 
 #Q5 Faça uma função que retorne o nome do álbum que mais vendeu e o que menos vendeu ao dar um ano de lançamento (retorne também o nome dos artistas correspondentes a cada álbum).
@@ -104,16 +122,19 @@ print(artistas1v())
 #Q8 Elabore uma função que retorna os 3 artistas que mais aparecem na planilha, para em seguida fazer um dataframe desses artistas com as colunas ARTISTA, TOTAL DE VENDAS onde a última coluna deve ser a soma de todas as vendas do artista. Faça por ordem decrescente de vendas.
 
 maxArtistas = function(){
+  #calcula a frequência de cada artista
   freq = as.data.frame(table(df["Artista"]))
-  freq = freq[!(freq$Freq < 3),]
-
+  #ordena freq pela frequência decrescente (-freq)
+  freq = freq[order(-freq$Freq),]
+  #retorna os 3 primeiros artistas que mais aparecem
   return(freq[1:3,])
-  
 }
+#armazena o retorno da funcao maxArtistas na variavel 3 maiores
+tresMaiores = maxArtistas()
+#calcula a soma das vendas dos 3 artistas retornados acima
+TotalVendas = c(sum(df[df$Artista == tresMaiores[1,1],][[5]]), sum(df[df$Artista == tresMaiores[2,1],][[5]]), sum(df[df$Artista == tresMaiores[3,1],][[5]]))
 
-TotalVendas = c(sum(df[df$Artista == 'DAY6',][[5]]), sum(df[df$Artista == 'RED VELVET',][[5]]), sum(df[df$Artista == 'THE BOYZ',][[5]]))
-
-vectArtistas = maxArtistas()[1]
+vectArtistas = tresMaiores[1]
 
 dfV = data.frame(vectArtistas, TotalVendas)
 dfV = dfV[order(-dfV$TotalVendas),]
